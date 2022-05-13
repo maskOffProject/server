2e87a5ae81c3a886d368455a8cb37048899f3074
Maya Sasson <mayasasson22@gmail.com>
2022-05-13 16:31:32 +0300

    fix: now works

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ from keras.applications.mobilenet_v2 import preprocess_input
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
-import argparse
 import cv2
-import os
 import matplotlib.pyplot as plt
 
 
+DETECTOR_MODEL_INPUT_SIZE = (224, 224)
+GENERATOR_MODEL_INPUT_SIZE = (128, 128)
+
+
 def generate_img(face_to_generate):
-    face_to_generate = process_image_to_input(face_to_generate, (128, 128))
+    face_to_generate = process_image_to_input(face_to_generate, GENERATOR_MODEL_INPUT_SIZE)
     generator_model = load_model('C:\dev\mask-off\server\generator_model/best.h5')
     prediction = generator_model.predict(face_to_generate)
-    # plt.imshow(prediction[0])
-    # plt.show()
 
     return prediction[0]
 
 
 def process_image_to_input(face, shape):
-    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
     face = cv2.resize(face, shape)
     face = img_to_array(face)
     face = preprocess_input(face)
@@ -38,6 +37,7 @@ def main():
     detector_model = load_model('C:\dev\mask-off\server\detector_model\detectorModel')
     # image = cv2.imread('C:\mask\with_mask\with-mask-default-mask-seed0000.png')
     image = cv2.imread('C:\mask\maksssksksss111.png')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # Detect the faces
     faces = find_faces(image)
 
@@ -53,27 +53,20 @@ def main():
         face_to_detect = face.copy()
         face_to_generate = face.copy()
 
-        face_to_detect = process_image_to_input(face_to_detect, (224, 224))
+        face_to_detect = process_image_to_input(face_to_detect, DETECTOR_MODEL_INPUT_SIZE)
 
         # pass the face through the model to determine if the face
         # has a mask or not
         (mask, withoutMask) = detector_model.predict(face_to_detect)[0]
-        # plt.imshow(face_to_detect[0])
-        # plt.show()
 
         if mask > withoutMask:
             prediction = generate_img(face_to_generate)
             plt.imshow(prediction)
             plt.show()
             prediction_origin_size = cv2.resize(prediction, (face.shape[0], face.shape[1]))
-            plt.imshow(prediction_origin_size)
-            plt.show()
-            image[startY:endY, startX:endX] = face
-            print(image.shape)
-            print(face.shape)
-            print(prediction_origin_size.shape)
+            prediction_origin_size = prediction_origin_size * 255
+            image[startY:endY, startX:endX] = prediction_origin_size
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     plt.imshow(image)
     plt.show()
 
