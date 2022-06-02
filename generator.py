1efed921b8af2f640d5b11cea91535f3541fd71d
elishadavidi <Elishadavidi@outlook.com>
2022-06-02 20:31:13 +0300

    add p2p and new trained models

--- generator.py
+++ generator.py
@@ -8,6 +8,11 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import base64
 
+from tensorflow.keras.preprocessing.image import load_img
+from numpy import expand_dims
+
+
+
 DETECTOR_MODEL_INPUT_SIZE = (224, 224)
 GENERATOR_MODEL_INPUT_SIZE = (128, 128)
 
@@ -32,6 +37,33 @@ def generate_img(face_to_generate):
     prediction = generator_model.predict(face_to_generate)
     return prediction[0]
 
+# load an image
+def load_image(filename, size=(256,256)):
+	# load image with the preferred size
+	pixels = load_img(filename, target_size=size)
+	# convert to numpy array
+	pixels = img_to_array(pixels)
+	# scale from [0,255] to [-1,1]
+	pixels = (pixels - 127.5) / 127.5
+	# reshape to 1 sample
+	pixels = expand_dims(pixels, 0)
+	return pixels
+
+def generate_img_p2p(face_to_generate):
+    # save photo
+    face_path = './face_to_generate.png'  
+    cv2.imwrite(face_path, cv2.cvtColor(face_to_generate, cv2.COLOR_RGB2BGR))
+    # load source image
+    src_image = load_image(face_path)
+    print('Loaded', src_image.shape)
+    # load model
+    model = load_model('./generator_model/model_040001.h5')
+    # generate image from source
+    gen_image = model.predict(src_image)
+    # scale from [-1,1] to [0,1]
+    gen_image = (gen_image + 1) / 2.0
+    # plot the image
+    return gen_image[0]
 
 def find_faces(image):
     face_cascade = cv2.CascadeClassifier('./detector_model/haarcascade_frontalface_default.xml')
@@ -92,6 +124,7 @@ def main(img):
             plt.imshow(face)
             plt.show()
             if mask > withoutMask:
+                # prediction = generate_img_p2p(face_to_generate)
                 prediction = generate_img(face_to_generate)
                 plt.imshow(prediction)
                 plt.show()
